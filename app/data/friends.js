73f05c252eb321725ce2b5f1acf7c9517297dd8e
Alex <adamm@urbn.com>
2019-02-21 23:35:45 -0500

    api routes and front end changes

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friendsArray = [
+    {
+        "name":"Mick Jagger",
+        "photo":"",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name":"Paul McCartney",
+        "photo":"",
+        "scores":[
+            1,
+            3,
+            2,
+            2,
+            1,
+            5,
+            5,
+            5,
+            4,
+            4
+          ]
+    },
+    {
+        "name":"Bob Dylan",
+        "photo":"",
+        "scores":[
+            4,
+            1,
+            1,
+            4,
+            4,
+            3,
+            3,
+            1,
+            1,
+            2
+          ]
+    },
+    {
+        "name":"Bruce Springsteen",
+        "photo":"",
+        "scores":[
+            3,
+            3,
+            2,
+            2,
+            5,
+            5,
+            3,
+            1,
+            1,
+            3
+          ]
+    }
+]
+
+module.exports = friendsArray;
---
+++ app/routing/apiroutes.js
@@ -0,0 +1,28 @@
+var friendData = require("../data/friends.js");
+
+module.exports = function(app) {
+    app.post("/api/friends", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newFriend = req.body;
+
+        friendDifferenceArray = [];
+    
+        for (var i=0; i<friendData.length; i++) {
+            var totalDifference = 0;
+            for (var j=0; j<friendData[i].scores.length; j++) {
+                var difference = Math.abs(friendData[i].scores[j] - req.body.scores[j]);
+                totalDifference += difference;
+            }
+            friendDifferenceArray.push(totalDifference);
+        }
+
+        console.log(friendDifferenceArray);
+        var winningScore = Math.min.apply(null, friendDifferenceArray);
+        var winningFriendIndex = friendDifferenceArray.indexOf(winningScore);
+    
+        //return the best match friend
+        res.json(friendData[winningFriendIndex]);
+        
+    });
+}
