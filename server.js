c5a3ca0eec1d2dd0b50e2ec4f06e94c8b0d83887
Alex <adamm@urbn.com>
2019-02-21 23:55:14 -0500

    removed api route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // require router files to define routes
-require("./app/routing/apiRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 // start the server
